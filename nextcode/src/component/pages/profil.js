ebec39eec07f471bfaad7077b40de893fcd92a09
Yacine9477 <ballistayacine@outlook.fr>
2025-03-25 16:17:18 +0100

    profil

---
+++ nextcode/src/component/pages/profil.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./profil.css";
+
+const Profil = () => {
+  return (
+    <div>
+      <h1>Profil Page</h1>
+      <p>Welcome to your profile page!</p>
+      {/* Add more profile details here */}
+    </div>
+  );
+};
+
+export default Profil;
