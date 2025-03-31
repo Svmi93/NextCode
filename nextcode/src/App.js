9cd0bb9e081dbc0843afe9d959d7f1902e1ce6c6
Yacine9477 <ballistayacine@outlook.fr>
2025-03-31 10:53:17 +0200

    nav barre et profil

--- nextcode/src/App.js
+++ nextcode/src/App.js
@@ -6,12 +6,34 @@ function App() {
   return (
     <Router>
       <nav>
-        <Link to="/profil">Profil</Link>
-        <Link to="/">Home</Link>
-        <Link to="/">Formations</Link>
-        <Link to="/">Inscription</Link>
-        <Link to="/">Connexion</Link>
-        <Link to="/">Profil</Link>
+        <h1>
+          NEXT<span>CODE</span>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/" class="navLink">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/" class="navLink">
+              Formations
+            </Link>
+          </li>
+          <li>
+            <Link to="/" class="navLink">
+              Inscription
+            </Link>
+          </li>
+          <li>
+            <Link to="/profil" class="btn">
+              Connexion
+            </Link>
+          </li>
+          {/* <li>
+            <Link to="/profil">Profil</Link>
+          </li> */}
+        </ul>
       </nav>
       <Routes>
         <Route path="/profil" element={<Profil />} />
